af6d0dc316a07b297b8d0385928b0fdd28ad5efd
Mayra Malaquias <mayra.malaquias@hotmail.com>
2023-03-28 20:19:27 -0300

    :wrench:

--- script.js
+++ script.js
@@ -17,8 +17,36 @@ function recebe() {
   dataExpCadastrado.innerHTML = dateExp.value;
   cvcCadastrado.innerHTML = cvc.value;
 }
-confirm.addEventListener("click", () => {
-  dados.classList.add("hidden");
-  register.classList.remove("hidden");
-  recebe();
+let cadastroCompleto = false;
+function valida() {
+  if (nome.value == "") {
+    alert("Preencha o campo nome");
+  } else if (number.value == "") {
+    alert("Preencha o campo number");
+  } else if (number.value.length > 20 || number.value[0] == 0) {
+    alert("Numero de cartao invalido");
+  } else if (number.value.length < 19) {
+    alert("Numero de cartao incompleto");
+  } else if (dateExp.value == "") {
+    alert("Preencha a data de validade do cartao");
+  } else if (cvc.value == "") {
+    alert("Preencha o campo cvc");
+  } else if (cvc.value.length > 3 || cvc.value[0] == 0) {
+    alert("cvc de cartao invalido");
+  } else if (cvc.value.length < 3) {
+    alert("cvc incompleto");
+  } else {
+    cadastroCompleto = true;
+  }
+}
+
+confirm.addEventListener("click", (e) => {
+  e.preventDefault();
+  valida();
+
+  if (cadastroCompleto) {
+    dados.classList.add("hidden");
+    register.classList.remove("hidden");
+    recebe();
+  }
 });
